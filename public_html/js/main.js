0e5c570460d1e89bcea07b14669d4191fa2656e5
Sasa <aleksdukovic@gmail.com>
2023-04-21 15:30:48 +0200

    mademoiselle half done

---
+++ public_html/js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+
+
+
+
+// animation start
+    function animation() {
+        var windowHight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var pozicija = $(this).offset().top;
+            var animacija = $(this).attr('data-animation');
+            var delay = $(this).attr('data-delay');
+            if (pozicija < scroll + windowHight - 50) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animacija);
+            }
+        });
+    }
+
+
+
+    animation();
+    $(window).scroll(function () {
+        animation();
+    });
+// animation end
+
+    $('.navbar-nav').on('click', '.toggle-submenu', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('fa-plus fa-minus');
+        $(this).parent().next('.submenu').slideToggle();
+    });
+    if ($('.team-slider').length > 0) {
+
+        $('.team-slider').owlCarousel({
+            loop: true,
+            margin: 25,
+            items: 1,
+            dots: true
+        });
+    }
+
+
+
+
+
+
+    if ($('.register-form').length > 0) {
+        $(function () {
+            $(".register-form").validate({
+                highlight: function (element) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                    $(element).closest('.form-group').addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
+                    $(element).closest('.form-group').addClass("is-valid").removeClass("is-invalid");
+                },
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    subject: {
+                        required: true
+                    },
+                    message: {
+                        required: true
+                    }
+
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                    error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
+                }
+
+            });
+        });
+    }//Form Validation
+
+
+
+});
